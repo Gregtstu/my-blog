4eaae05dc8ab093181382628b0c54cd7a38c5403
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-04 15:36:33 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './user/shared/main-layout/main-layout.component';
+import { ArticlePageComponent } from './user/article-page/article-page.component';
+import { CreatePageComponent } from './user/create-page/create-page.component';
+import { FavoritePageComponent } from './user/favorite-page/favorite-page.component';
+import { MainPageComponent } from './user/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    ArticlePageComponent,
+    CreatePageComponent,
+    FavoritePageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
