f1e25235b98483327be935e5cdb5b7c74fbf747b
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-04 16:07:28 +0300

    added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './user/shared/main-layout/main-layout.component';
 import { ArticlePageComponent } from './user/article-page/article-page.component';
@@ -9,6 +8,11 @@ import { CreatePageComponent } from './user/create-page/create-page.component';
 import { FavoritePageComponent } from './user/favorite-page/favorite-page.component';
 import { MainPageComponent } from './user/main-page/main-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from "./app-routing.module";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
