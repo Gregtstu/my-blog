02aa1bdc63e4568f972dfed3eada2c61b2143fa5
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-04 15:41:39 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ArticlePageComponent} from "./user/article-page/article-page.component";
+import {MainLayoutComponent} from "./user/shared/main-layout/main-layout.component";
+import {MainPageComponent} from "./user/main-page/main-page.component";
+import {FavoritePageComponent} from "./user/favorite-page/favorite-page.component";
+import {CreatePageComponent} from "./user/create-page/create-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component:MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: MainPageComponent},
+      {path: 'favorite', component: FavoritePageComponent},
+      {path: 'create', component: CreatePageComponent},
+      {path: 'article/:id', component: ArticlePageComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
